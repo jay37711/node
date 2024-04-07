05325c3edb4d928a9664cfd2fd679ecb39460fb6
jay37711 <162629047+jay37711@users.noreply.github.com>
2024-04-07 09:44:43 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,16 +1,17 @@
 const mysql = require('mysql');
 const os = require('os');
 const hostname = os.hostname();
-if(hostname == 'localhost'){
-  var host = 'localhost'
-  var user = 'root'
-  var password = ''
-  var databse = 'chit_chat'
-}else{
+if(hostname == 'srv-co8oso6v3ddc73flq45g-hibernate-b484d88f6-tjvbk'){
+
   var host = 'bkormmq4wmbzhzke70o5-mysql.services.clever-cloud.com'
   var user = 'utmkt20gg2hzslro'
   var password = 'E2OU0cQ37eY0w0fj7BCm'
   var databse = 'bkormmq4wmbzhzke70o5'
+}else{
+  var host = 'localhost'
+  var user = 'root'
+  var password = ''
+  var databse = 'chit_chat'
 }
 
 const db = mysql.createConnection({
